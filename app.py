2f81b02e98ace40f02f28a84275f1fb7835987cb
Sahil <saaahiillbackup@gmail.com>
2024-12-21 13:08:00 +0530

    removed clutter

--- app.py
+++ app.py
@@ -35,20 +35,5 @@ def gallery():
 def contact():
     return render_template('contact.html')
 
-@app.route('/submit_donation', methods=['POST'])
-def submit_donation():
-    name = request.form['name']
-    # email = request.form['email']
-    # phone = request.form['phone']
-    # amount = request.form['amount']
-    # message = request.form['message']
-    # # Process the form data here
-    return redirect(url_for('thank_you', name=name))
-
-@app.route('/thank_you')
-def thank_you():
-    name = request.args.get('name')
-    return render_template('thank_you.html', name=name)
-
 if __name__ == '__main__':
     app.run(debug=True)
